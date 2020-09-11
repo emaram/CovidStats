8febb7fef20a0fcb0eacb545ccb9ad371d6e43cf
Eugen Marin <eugen.marin@outlook.com>
2020-09-11 12:33:34 +0300

    Initial commit

---
+++ src/CovidStatsAPI/Controllers/CovidStatsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CovidStatsAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CovidInfoController : ControllerBase
+    {
+        private readonly ILogger<CovidInfoController> _logger;
+
+        public CovidInfoController(ILogger<CovidInfoController> logger)
+        {
+            _logger = logger;
+        }
+
+		// GET covidinfo
+        [HttpGet]
+        public IEnumerable<string[]> Get()
+        {
+			CovidInfo info = new CovidInfo();
+			return info.GetInfo("");
+        }
+
+		// GET covidinfo/Romania
+        [HttpGet("{country}")]
+        public IEnumerable<string[]> Get(string country)
+        {
+			CovidInfo info = new CovidInfo();
+			return info.GetInfo(country);
+        }
+
+    }
+}
---
+++ src/CovidStatsAPI/CovidInfo.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace CovidStatsAPI
+{
+    public class CovidInfo
+    {
+        private void DownloadCovidInfoFile(string url, string filename)
+		{
+			HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+    		HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+    		StreamReader sr = new StreamReader(resp.GetResponseStream());
+    		string results = sr.ReadToEnd();
+    		sr.Close();
+
+			StreamWriter sw = new StreamWriter(filename, false);
+			sw.WriteLine(results);
+			sw.Close();
+		}
+
+		public IEnumerable<string[]> GetInfo(string country) 
+		{
+			this.DownloadCovidInfoFile("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", "covid.csv");
+
+			List<string[]> result = new List<string[]>();
+			using (StreamReader sr = new StreamReader("covid.csv")) 
+			{
+				// Add header row
+				result.Add(sr.ReadLine().Split(','));
+
+				// Now add the other rows
+				while (!sr.EndOfStream)
+				{
+					string[] row = sr.ReadLine().Split(',');
+					if (row.Length > 8)
+					{
+						if (country.Trim().Length == 0)
+							result.Add(row);
+						else
+						{
+							if (country.ToUpper() == row[8].ToUpper())
+								result.Add(row);
+						}
+					}
+				}
+			}
+
+			return result;
+		}
+    }
+}
