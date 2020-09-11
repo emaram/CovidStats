ad588ec0f814ba3a96e8fbe91946b86efc60e081
Eugen Marin <eugen.marin@outlook.com>
2020-09-11 17:17:35 +0300

    Added unit tests

--- src/CovidStatsAPI/Controllers/CovidStatsController.cs
+++ src/CovidStatsAPI/Controllers/CovidStatsController.cs
@@ -7,32 +7,35 @@ using Microsoft.Extensions.Logging;
 
 namespace CovidStatsAPI.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class CovidInfoController : ControllerBase
-    {
-        private readonly ILogger<CovidInfoController> _logger;
+	[ApiController]
+	[Route("[controller]")]
+	public class CovidInfoController : ControllerBase
+	{
+		private readonly ILogger<CovidInfoController> _logger;
 
-        public CovidInfoController(ILogger<CovidInfoController> logger)
-        {
-            _logger = logger;
-        }
-
+		public CovidInfoController(ILogger<CovidInfoController> logger)
+		{
+			_logger = logger;
+		}
+		public CovidInfoController()
+		{
+			
+		}
 		// GET covidinfo
-        [HttpGet]
-        public IEnumerable<string[]> Get()
-        {
+		[HttpGet]
+		public IEnumerable<string[]> Get()
+		{
 			CovidInfo info = new CovidInfo();
 			return info.GetInfo("");
-        }
+		}
 
 		// GET covidinfo/Romania
-        [HttpGet("{country}")]
-        public IEnumerable<string[]> Get(string country)
-        {
+		[HttpGet("{country}")]
+		public IEnumerable<string[]> Get(string country)
+		{
 			CovidInfo info = new CovidInfo();
 			return info.GetInfo(country);
-        }
+		}
 
-    }
+	}
 }
--- test/CovidStatsAPI.Test/UnitTest1.cs
+++ test/CovidStatsAPI.Test/UnitTest1.cs
@@ -1,14 +1,38 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
+using CovidStatsAPI.Controllers;
 
 namespace CovidStatsAPI.Test
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+		CovidInfoController controller = new CovidInfoController();
 
-        }
+		[Fact]
+		public void DataHeaderExists()
+		{
+			List<string[]> returnValue = new List<string[]>(controller.Get("ROU"));
+			if (returnValue.Count == 0)
+				Assert.Equal(1, 0);	// If no values, test fails
+			
+			string[] header = returnValue[0];
+			Assert.Equal("dateRep", header[0]);
+		}
+
+		[Fact]
+		public void TodayDataExists()
+		{
+			List<string[]> returnValue = new List<string[]>(controller.Get("ROU"));
+			if (returnValue.Count == 0)
+				Assert.Equal(1, 0);		// If no values, test fails
+			
+			string[] todayData = returnValue[1];
+			
+			string expectedDate = DateTime.Now.Year.ToString() + "/" + DateTime.Now.Month.ToString() + "-" + DateTime.Now.Day.ToString();
+			string actualDate = todayData[3] + "-" + todayData[2] + "-" + todayData[1];
+			
+			Assert.Equal(expectedDate, actualDate);
+		}
     }
 }
